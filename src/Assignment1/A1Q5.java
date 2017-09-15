7da8dd545f68398c954a3b2110678bfc50496942
hek6452 <hek6452@HRH-IC0027327.wrdsb.ca>
2017-09-15 14:12:36 -0400

    First Assignment

--- src/Assignment1/A1Q5.java
+++ src/Assignment1/A1Q5.java
@@ -14,6 +14,6 @@ public class A1Q5 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Create City
     }
 }
