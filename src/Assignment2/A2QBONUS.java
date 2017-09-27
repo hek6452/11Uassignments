45c67bf15375d9aae232a0635a2fb69de7865d64
hek6452 <hek6452@HRH-IC0027327.wrdsb.ca>
2017-09-27 14:03:44 -0400

    First Assignment

---
+++ src/Assignment2/A2QBONUS.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment2;
+
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.Robot;
+
+/**
+ *
+ * @author hek6452
+ */
+public class A2QBONUS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Create City
+        City javatown = new City();
+                //create robot
+                Robot mac=new Robot(javatown,1,1,Direction.WEST);
+                
+    
+    }
+}
