b4de27b39186d6a9822cf49cde338dc43d99ecae
hek6452 <hek6452@HRH-IC0027327.wrdsb.ca>
2017-10-24 13:18:49 -0400

    Thats a good meatball

---
+++ src/StringsAssignment/UbbiBubbi.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package StringsAssignment;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author hek6452
+ */
+public class UbbiBubbi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter word to be translated: ");
+        
+    }
+}
