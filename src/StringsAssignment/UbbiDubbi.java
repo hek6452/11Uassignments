c2edaefac3490482eda329e38e27c78220ed706c
hek6452 <hek6452@HRH-IC0027327.wrdsb.ca>
2017-11-08 12:58:40 -0500

    Thats a good meatball

--- src/StringsAssignment/UbbiDubbi.java
+++ src/StringsAssignment/UbbiDubbi.java
@@ -19,35 +19,41 @@ public class UbbiDubbi {
         // TODO code application logic here
         int p = 0;
         while (p < 1) {
+            //Get word from user
             Scanner input = new Scanner(System.in);
             System.out.println("Please enter word to be translated: ");
             String word = input.nextLine();
+            //Allows word to start with both lowercase and uppercase
             word=word.toLowerCase();
+            //if word is "EXIT", prints out farewell and terminates program
             String exit = "EXIT";
             if (word.equals(exit)) {
                 System.out.println("See you in Narnia lightning Macqueen");
                 break;
             }
 
-
+           //blank string translate and foundvowel boolean=false
             String translate = "";
             Boolean foundvowel = false;
 
-
+            //replaces the vowel at it's specific location
             for (int i = 0; i < word.length(); i++) {
+                //if letter is a vowel and the found vowel is false then translate
                 if ((word.charAt(i) == 'a' || word.charAt(i) == 'e' || word.charAt(i) == 'i'
                         || word.charAt(i) == 'o' || word.charAt(i) == 'u') && foundvowel == false) {
-
+            
                     translate = translate + "ub";
                     translate = translate + word.charAt(i);
-
+                    //sets foundvowel to true
                     foundvowel = true;
                 } else {
+                    //if the letter is a vowel and  foundvowel is true then a vowel will be added to translate
                     if ((word.charAt(i) == 'a' || word.charAt(i) == 'e' || word.charAt(i) == 'i'
                             || word.charAt(i) == 'o' || word.charAt(i) == 'u') && foundvowel == false) {
 
                         translate = translate + word.charAt(i);
                     } else {
+                        //then letter vowel is added to the translate and foundvowel=false
                         translate = translate + word.charAt(i);
                         foundvowel = false;
                     }
@@ -55,6 +61,7 @@ public class UbbiDubbi {
                 }
 
             }
+            //output answer
             System.out.println(translate);
             p = p + 1;
         }
