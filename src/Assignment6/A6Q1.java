b38817879f3e39932983c51e5dab32bf2a6237c7
hek6452 <hek6452@HRH-IC0027327.wrdsb.ca>
2017-11-08 13:19:44 -0500

    Thats a good meatball

---
+++ src/Assignment6/A6Q1.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment6;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author hek6452
+ */
+public class A6Q1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       
+        // create scanner for input
+        Scanner input = new Scanner(System.in);
+        
+         double[] marks = new double[6];
+         
+         // create array to store name of expenses
+        String[] expenseNames = new String[6];
+    }
+}
