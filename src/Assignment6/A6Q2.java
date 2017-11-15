4822433f1c12b8f47dca645f01fdbd6db7245244
hek6452 <hek6452@HRH-IC0027327.wrdsb.ca>
2017-11-15 14:23:18 -0500

    Thats a good meatball

--- src/Assignment6/A6Q2.java
+++ src/Assignment6/A6Q2.java
@@ -10,37 +10,38 @@ import java.util.Scanner;
  *
  * @author hek6452
  */
-public class A6Q1 {
+public class A6Q2 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        //asks for the amount of students in the class
 
-
-
-        // create array to store name of marks
         Scanner input = new Scanner(System.in);
         System.out.println("How many people are in your class?");
         int students = input.nextInt();
         double[] classnum = new double[students];
 
 
+
+        //Get heights then place into array
         for (int i = 0; i < classnum.length; i++) {
-            System.out.println("What are your averages?");
+            System.out.println("What are your heights in centimetres?");
             classnum[i] = input.nextDouble();
 
+
+
         }
-        //Taking all marks and adding together
+        //adds all marks together
         double average = 0;
         for (int i = 0; i < classnum.length; i++) {
-            average = average + classnum[i] / students;
-     
+            average = average + classnum[i]/students;
+
         }
-     
-
-        System.out.println("The class average is " + average);
-
+        //calculating the total averages
+        double averages = average;
+        System.out.println("Your class average height is "+averages+" centimetres");
     }
 }
---
+++ src/Assignment6/A6Q3.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment6;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author hek6452
+ */
+public class A6Q3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Ask how many numbers are being sorted
+        Scanner input = new Scanner(System.in);
+        System.out.println("How many numbers are being sorted?");
+        int numbers = input.nextInt();
+        double[] numnumbers = new double[numbers];
+    }
+}
