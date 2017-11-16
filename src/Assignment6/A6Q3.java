e2c0d46ca83b87a5159515526d9f0ae9b73ad465
hek6452 <hek6452@HRH-IC0027327.wrdsb.ca>
2017-11-16 13:17:34 -0500

    Thats a good meatball

--- src/Assignment6/A6Q3.java
+++ src/Assignment6/A6Q3.java
@@ -16,10 +16,27 @@ public class A6Q3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //Ask how many numbers are being sorted
+        //create array for numbers
         Scanner input = new Scanner(System.in);
-        System.out.println("How many numbers are being sorted?");
-        int numbers = input.nextInt();
-        double[] numnumbers = new double[numbers];
+
+        double[] numbers = new double[2];
+   
+
+        //create array for numbers input
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("What are your numbers?");
+            numbers[i] = input.nextDouble();
+        }
+            //sort numbers
+            double temp=0;
+            if (numbers[0]>numbers[1]){
+                temp=numbers[0];
+                numbers[0]=numbers[1];
+                numbers[1]=temp;
+
+            }
+            System.out.println("Your numbers in order are "+numbers[0] + ", " + numbers[1]);
+
+        }
     }
-}
+
