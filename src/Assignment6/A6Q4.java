f4ca7748c61d26fa7e26ac1903606c235a3d03d6
hek6452 <hek6452@HRH-IC0027327.wrdsb.ca>
2017-11-16 13:19:11 -0500

    Thats a good meatball

---
+++ src/Assignment6/A6Q4.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment6;
+
+/**
+ *
+ * @author hek6452
+ */
+public class A6Q4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
