f5828d7bdb7426b8ca07404b0f79736624bfd46f
hek6452 <hek6452@HRH-IC0027327.wrdsb.ca>
2017-11-20 13:13:21 -0500

    Thats a good meatball

--- src/Assignment6/A6Q4.java
+++ src/Assignment6/A6Q4.java
@@ -4,6 +4,8 @@
  */
 package Assignment6;
 
+import java.util.Scanner;
+
 /**
  *
  * @author hek6452
@@ -15,5 +17,34 @@ public class A6Q4 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        //ask for how many people in class, this question will be for 10
+        System.out.println("How many people are in your class?");
+        int students = input.nextInt();
+        double[] classnum = new double[students];
+        //Ask for the marks
+        for (int i = 0; i < classnum.length; i++) {
+            System.out.println("What are your averages?");
+            classnum[i] = input.nextDouble();
+        }
+        //Sort averages for 
+        for (int i = 0; i < classnum.length - 1; i++) {
+            for (int j = i + 1; j < classnum.length; j++) {
+                //compare two numbers
+                if (classnum[i] > classnum[j]) {
+                    double temp = 0;
+                        temp = classnum[i];
+                        classnum[i] = classnum[j];
+                        classnum[j] = temp;
+                    
+                    
+                }
+            }
+
+        }
+        System.out.println("Your marks in order are:" );
+        for (int i = 0; i < classnum.length; i++) {
+            System.out.println(classnum[i]);
+        }
     }
 }
