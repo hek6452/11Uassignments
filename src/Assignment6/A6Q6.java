010e75e627a54f02bb1769f704b3718eeacefba0
hek6452 <hek6452@HRH-IC0027327.wrdsb.ca>
2017-11-22 13:18:45 -0500

    Thats a good meatball

--- src/Assignment6/A6Q6.java
+++ src/Assignment6/A6Q6.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
  *
  * @author hek6452
  */
-public class A6Q5 {
+public class A6Q6 {
 
     /**
      * @param args the command line arguments
@@ -18,15 +18,23 @@ public class A6Q5 {
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner input = new Scanner(System.in);
-        //ask for how many people in class, this question will be for 10
         System.out.println("How many people are in your class?");
         int students = input.nextInt();
         double[] classnum = new double[students];
-        //Ask for the marks
+
+
         for (int i = 0; i < classnum.length; i++) {
             System.out.println("What are your averages?");
             classnum[i] = input.nextDouble();
+
         }
+        //Taking all marks and adding together
+        double average = 0;
+        for (int i = 0; i < classnum.length; i++) {
+            average = average + classnum[i] / students;
+
+        }
+        System.out.println("The class average is " + average);
         //Sort averages for 
         for (int i = 0; i < classnum.length - 1; i++) {
             for (int j = i + 1; j < classnum.length; j++) {
@@ -36,35 +44,13 @@ public class A6Q5 {
                     temp = classnum[i];
                     classnum[i] = classnum[j];
                     classnum[j] = temp;
-
-
                 }
-            }
 
+            }
         }
         System.out.println("Your marks in order are:");
         for (int i = 0; i < classnum.length; i++) {
             System.out.println(classnum[i]);
-
         }
-        System.out.println("");
-        //if remainder is one set as integer mid
-        if (classnum.length % 2 == 1) {
-            int mid = classnum.length / 2;
-            System.out.println(classnum[mid]);
-        }else
-        //otherwise, second middle number is mid minus 1, remainder taken away
-        {
-            int mid = classnum.length / 2;
-            int mid2 = mid - 1;
-            //print out median of numbers given
-            System.out.println((classnum[mid]+classnum[mid2])/2.0);
-            
-            
-        }
-
-
-
-
     }
 }
